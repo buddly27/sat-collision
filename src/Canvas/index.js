dbaacf0ecee555fcc60a7ef4c3c4a060e2cde1d5
jeremyr <jeremyr@themill.com>
2020-05-25 20:13:17 -0700

    First commit

---
+++ src/Canvas/index.js
@@ -0,0 +1,255 @@
+import React from "react";
+import "./style.css";
+
+
+class Canvas extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.canvas = React.createRef();
+        this.state = {
+            gridSize: 20,
+            originX: Math.round(window.innerWidth / 2.0),
+            originY: Math.round(window.innerHeight / 2.0),
+            width: window.innerWidth,
+            height: window.innerHeight,
+        };
+
+        // Record origin pressed when moving canvas;
+        this.isDrag = false;
+    }
+
+    componentDidMount() {
+        window.addEventListener("resize", this.updateSize);
+        this.draw();
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener("resize", this.updateSize);
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        this.draw();
+    }
+
+    updateSize = () => {
+        this.setState({
+            width: window.innerWidth,
+            height: window.innerHeight,
+        });
+    };
+
+    onMouseDown = () => {
+        this.isDrag = true;
+    };
+
+    onMouseUp = () => {
+        this.isDrag = false
+    };
+
+    onMouseMove = (event) => {
+        if (! this.isDrag)
+            return;
+
+        this.setState({
+            originX: event.clientX,
+            originY: event.clientY,
+        });
+    };
+
+    draw = () => {
+        const {gridSize, originX, originY} = this.state;
+        drawAxis(this.canvas.current, gridSize, originX, originY);
+
+        const vertices = [
+            [1.5, 0], [3, 4], [10, 4], [12, 0], [10, -4], [3, -4]
+        ];
+
+        drawPolygon(this.canvas.current, gridSize, originX, originY, vertices)
+    };
+
+    render() {
+        return (
+            <canvas
+                ref={this.canvas}
+                className="Canvas"
+                onMouseDown={this.onMouseDown}
+                onMouseUp={this.onMouseUp}
+                onMouseMove={this.onMouseMove}
+                onTouchStart={this.onMouseDown}
+                onTouchEnd={this.onMouseUp}
+                onTouchMove={this.onMouseMove}
+                onKeyDown={onKeyDown}
+                width={this.state.width}
+                height={this.state.height}
+                tabIndex="0"
+            />
+        );
+    }
+}
+
+
+const drawAxis = (canvas, gridSize, originX, originY) => {
+    const context = canvas.getContext("2d");
+    context.clearRect(0, 0, canvas.width, canvas.height);
+
+    context.lineWidth = 1;
+    context.strokeStyle = "#e9e9e9";
+    context.fillStyle = '#000';
+
+    // Draw grid lines along X axis.
+    for (let index = 0;; index += 1) {
+        const y = originY + (gridSize * index + 0.5);
+        if (y > Math.floor(canvas.height))
+            break;
+
+        context.beginPath();
+        context.moveTo(0, y);
+        context.lineTo(canvas.width, y);
+        context.stroke();
+    }
+
+    for (let index = 1;; index += 1) {
+        const y = originY - (gridSize * index + 0.5);
+        if (y < 0)
+            break;
+
+        context.beginPath();
+        context.moveTo(0, y);
+        context.lineTo(canvas.width, y);
+        context.stroke();
+    }
+
+    // Draw grid lines along Y axis.
+    for (let index = 0;; index += 1) {
+        const x = originX + (gridSize * index + 0.5);
+        if (x > Math.floor(canvas.width))
+            break;
+
+        context.beginPath();
+        context.moveTo(x, 0);
+        context.lineTo(x, canvas.height);
+        context.stroke();
+    }
+
+    for (let index = 1;; index += 1) {
+        const x = originX - (gridSize * index + 0.5);
+        if (x < 0)
+            break;
+
+        context.beginPath();
+        context.moveTo(x, 0);
+        context.lineTo(x, canvas.height);
+        context.stroke();
+    }
+
+    context.strokeStyle = "#888";
+
+    // Draw origin lines
+    context.beginPath();
+    context.moveTo(0, originY + 0.5);
+    context.lineTo(canvas.width, originY + 0.5);
+    context.stroke();
+
+    context.beginPath();
+    context.moveTo(originX + 0.5, 0);
+    context.lineTo(originX + 0.5, canvas.height);
+    context.stroke();
+
+    // Ticks marks along the X axis.
+    for (let index = 1;; index += 1) {
+        const x = originX + (gridSize * index + 0.5);
+        if (x > Math.floor(canvas.width))
+            break;
+
+        context.beginPath();
+        context.moveTo(x, originY -3);
+        context.lineTo(x, originY + 3);
+        context.stroke();
+
+        // Draw numbers.
+        context.font = "9px Arial";
+        context.textAlign = "start";
+        context.fillText(`${index}`, x - 2, originY + 15);
+    }
+
+    for (let index = 1;; index += 1) {
+        const x = originX - (gridSize * index + 0.5);
+        if (x < 0)
+            break;
+
+        context.beginPath();
+        context.moveTo(x, originY -3);
+        context.lineTo(x, originY + 3);
+        context.stroke();
+
+        // Draw numbers.
+        context.font = "9px Arial";
+        context.textAlign = "start";
+        context.fillText(`${-index}`, x - 2, originY + 15);
+    }
+
+    // Ticks marks along the Y axis.
+    for (let index = 1;; index += 1) {
+        const y = originY + (gridSize * index + 0.5);
+        if (y > Math.floor(canvas.height))
+            break;
+
+        context.beginPath();
+        context.moveTo(originX - 3, y);
+        context.lineTo(originX + 3, y);
+        context.stroke();
+
+        // Draw numbers.
+        context.font = "9px Arial";
+        context.textAlign = "start";
+        context.fillText(`${-index}`, originX + 8, y + 2);
+    }
+
+    for (let index = 1;; index += 1) {
+        const y = originY - (gridSize * index + 0.5);
+        if (y < 0)
+            break;
+
+        context.beginPath();
+        context.moveTo(originX - 3, y);
+        context.lineTo(originX + 3, y);
+        context.stroke();
+
+        // Draw numbers.
+        context.font = "9px Arial";
+        context.textAlign = "start";
+        context.fillText(`${index}`, originX + 8, y + 2);
+    }
+};
+
+
+const drawPolygon = (canvas, gridSize, originX, originY, vertices) => {
+    const context = canvas.getContext("2d");
+    context.strokeStyle = "#888";
+    context.fillStyle = '#f00';
+
+    context.beginPath();
+    let vertex = vertices.shift();
+    context.moveTo(
+        originX + gridSize * vertex[0],
+        originY + gridSize * vertex[1]
+    );
+
+    vertices.forEach((vertex) => {
+        context.lineTo(
+            originX + gridSize * vertex[0],
+            originY + gridSize * vertex[1]
+        );
+    });
+
+    context.closePath();
+    context.fill();
+    context.stroke();
+};
+
+function onKeyDown(_) {
+    console.log("Keydown...");
+}
+
+export default Canvas;
