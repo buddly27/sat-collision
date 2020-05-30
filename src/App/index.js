8e48cd7981c574840172707cd7af4a438f5c01e6
Jeremy Retailleau <jeremy.retailleau@gmail.com>
2020-05-29 19:20:26 -0700

    Add application bar

--- src/App/index.js
+++ src/App/index.js
@@ -1,13 +1,33 @@
 import React from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import IconButton from "@material-ui/core/IconButton";
+import Typography from "@material-ui/core/Typography";
+import MenuIcon from "@material-ui/icons/Menu";
 import Canvas from "../Canvas/index.js";
 
 
 function App() {
     return (
         <div className="App">
-            <Canvas />
+            <AppBar position="static">
+                <Toolbar>
+                    <IconButton
+                        edge="start"
+                        color="inherit"
+                        aria-label="open drawer"
+                    >
+                        <MenuIcon/>
+                    </IconButton>
+                    <Typography variant="h6" noWrap>
+                        Collision demonstration
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+            <Canvas/>
         </div>
     );
 }
 
 export default App;
+
